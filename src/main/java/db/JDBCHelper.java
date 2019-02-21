e38a46f18d6e791e3dbe44b25805d17c0cc2c3be
kgalli <mail@kgalli.de>
2019-02-21 13:16:13 +0100

    Refactor JDBC helper use try with resources

--- src/main/java/db/JDBCHelper.java
+++ src/main/java/db/JDBCHelper.java
@@ -12,52 +12,28 @@ public class JDBCHelper {
     private static Logger logger = LoggerFactory.getLogger(JDBCHelper.class);
 
     public static Optional<Book> getBook(String isbn) {
-        Connection connObj = null;
-        PreparedStatement prepStmtObj = null;
-        ResultSet resultSetObj = null;
-        try {
-            connObj = getConnection();
-
-            PreparedStatement prepStatement = connObj.prepareStatement("SELECT * FROM books WHERE isbn = ? LIMIT 1;");
-            prepStatement.setString(1, isbn);
+        try (var con = getConnection();
+            var prepStatement = con.prepareStatement("SELECT * FROM books WHERE isbn = ? LIMIT 1;")) {
 
             logger.info(prepStatement.toString());
-
-            ResultSet resObj = prepStatement.executeQuery();
+            prepStatement.setString(1, isbn);
             Book book = null;
 
-            while(resObj.next()) {
-                book = new Book(
-                        resObj.getString("title"),
-                        resObj.getString("author"),
-                        resObj.getString("isbn")
-                );
+            try(var resObj = prepStatement.executeQuery()) {
+                while(resObj.next()) {
+                    book = new Book(
+                            resObj.getString("title"),
+                            resObj.getString("author"),
+                            resObj.getString("isbn")
+                    );
+                }
             }
 
             return Optional.ofNullable(book);
         }
          catch (Exception sqlException) {
             sqlException.printStackTrace();
-
             return Optional.empty();
-        }
-        finally {
-            try {
-                // Close Result Set Object
-                if(resultSetObj!=null) {
-                    resultSetObj.close();
-                }
-                // Close Prepared Statement Object
-                if(prepStmtObj!=null) {
-                    prepStmtObj.close();
-                }
-                // Close Connection Object
-                if(connObj!=null) {
-                    connObj.close();
-                }
-            } catch (SQLException sqlException) {
-                sqlException.printStackTrace();
-            }
         }
     }
 
