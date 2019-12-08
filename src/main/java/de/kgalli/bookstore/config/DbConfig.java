7406cb965a129eff1dfaf68ab062641a226b4601
kgalli <mail@kgalli.de>
2019-12-08 16:17:26 +0100

    Add system variables support for db config

--- src/main/java/de/kgalli/bookstore/config/DbConfig.java
+++ src/main/java/de/kgalli/bookstore/config/DbConfig.java
@@ -8,23 +8,35 @@ public class DbConfig {
     private String database = "kgalli";
     private String dbUrl = String.format(
             "jdbc:postgresql://%s:%s/%s?user=%s&password=%s",
-            host,
-            port,
-            database,
-            user,
-            password
+            getHost(),
+            getPort(),
+            getDatabase(),
+            getUser(),
+            getPassword()
     );
 
     public String getUrl() {
         return dbUrl;
     }
 
+    public String getHost() {
+        return getEnvOrDefaultValue("DB_HOST", host);
+    }
+
+    public int getPort() {
+        return getEnvOrDefaultValue("DB_PORT", port);
+    }
+
     public String getUser() {
-        return user;
+        return getEnvOrDefaultValue("DB_USER", user);
     }
 
     public String getPassword() {
-        return password;
+        return getEnvOrDefaultValue("DB_PASSWORD", password);
+    }
+
+    public String getDatabase() {
+        return getEnvOrDefaultValue("DB_DATABASE", database);
     }
 
     public void setUser(String user) {
@@ -37,5 +49,19 @@ public class DbConfig {
 
     public void setDbUrl(String dbUrl) {
         this.dbUrl = dbUrl;
+    }
+
+    private String getEnvOrDefaultValue(String name, String defaultValue) {
+        var envValue = System.getenv(name);
+
+        return  envValue != null ? envValue : defaultValue;
+    }
+
+    private int getEnvOrDefaultValue(String name, int defaultValue) {
+        var envValue = System.getenv(name);
+
+        return  (envValue != null)
+                ? Integer.valueOf(envValue)
+                : defaultValue;
     }
 }
